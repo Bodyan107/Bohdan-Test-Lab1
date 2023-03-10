9b14bba553a11f5c89b121ff418b72dbf157aca6
bodyan107 <tbodya107@gmail.com>
2023-03-10 19:12:49 +0200

    my second commit

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Slava Ukraini")
+#class Student:
+ #   print("Hi")
+  #  def __init__(self, height=160):
+   #     self.height = height
+    #    self.name = "Bohdan"
+
+#nick = Student()
+#kate = Student(height=170)
+#print(kate.height)
+
+class Student:
+    amount_of_students = 0
+    def init(self, height=160):
+        self.height = height
+        Student.amount_of_students+=1
+    def grow(self, height=1):
+        self.height+=height
+nick = Student()
+kate = Student(height=170)
+nick.grow(height=15)
+print(kate.height)
+print(nick.height)
